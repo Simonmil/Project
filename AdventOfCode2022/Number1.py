5159ffd5abe27a92221f7231a40d4c8cad62c7c1
Simon Millerjord <simonmillerjord@gmail.com>
2024-04-18 16:49:40 -0700

    Finished first task and startet the second

--- AdventOfCode2022/Number1.py
+++ AdventOfCode2022/Number1.py
@@ -1,1 +1,22 @@
-import numpy
+import numpy as np
+
+file  = open("CaloriesList.txt")
+Elves = []
+Cals = 0
+
+for line in file:
+    try:
+        Cals += int(line)
+    except:
+        Elves.append(Cals)
+        Cals = 0
+
+print(max(Elves))
+
+# Part 2
+
+ElvesSorted = sorted(Elves,reverse=True)
+#print(ElvesSorted)
+
+TopThreeCalsSum = sum(ElvesSorted[0:3])
+print(TopThreeCalsSum)
