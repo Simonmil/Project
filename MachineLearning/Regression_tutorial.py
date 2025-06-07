bf9fec2acf01ed8432f009216d502daf8779f9fa
Simon Millerjord <simonmillerjord@gmail.com>
2025-06-07 09:54:39 -0700

    Want to try to follow this tutorial. Gonna comment and read into things along the way. No just do, always think!

---
+++ MachineLearning/Regression_tutorial.py
@@ -0,0 +1,12 @@
+import os;  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import numpy as np
+import matplotlib as plt
+import pandas as pd
+import seaborn as sns
+
+
+# Make NumPy printouts easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
