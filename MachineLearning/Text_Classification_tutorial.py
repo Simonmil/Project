037f9ddaedb4dbec8701fc610978ee4ef78067fb
Simon Millerjord <simonmillerjord@gmail.com>
2025-07-25 14:32:28 -0700

    Starting adding dataset

--- MachineLearning/Text_Classification_tutorial.py
+++ MachineLearning/Text_Classification_tutorial.py
@@ -17,13 +17,33 @@ The model will resolve if a review is positive or negative.
 url = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
 dataset = tf.keras.utils.get_file("aclImdb_v1", url, untar=True, cache_dir='.',cache_subdir='') # cache_dir is the directory to cache the dataset, . means 'here'?
-dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb')
+dataset_dir = os.path.join(os.path.dirname(dataset), 'aclImdb_v1','aclImdb')
+
+#print(os.listdir(dataset_dir))
+train_dir = os.path.join(dataset_dir,'train')  
+print(os.listdir(train_dir))
 
 
-os.listdir(dataset_dir)
+sample_file = os.path.join(train_dir, 'pos/1181_9.txt')
+with open(sample_file) as f:
+    print(f.read())
 
 
+remove_dir = os.path.join(train_dir, 'unsup')
+shutil.rmtree(remove_dir)
 
+batch_size = 32
+seed = 42
 
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed
+)
 
-
+for text_batch, label_batch in raw_train_ds.take(1):
+    for i in range(3):
+        print("Review",text_batch.numpy()[i])
+        print("Label",label_batch.numpy()[i])
